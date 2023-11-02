c8a46816d0fb5f60d03df6d895f060c1f9d5206e
Ricardo Vidal Ortiz <ricardovidalortiz@gmail.com>
2023-11-02 19:57:03 +0100

    desarrollo de la web v1

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -10,7 +10,49 @@
  * Write any other JavaScript below
  */
 
-+( function() {
+// Obtener los elementos del DOM
+const portada = document.querySelector(".portada");
+const imagenes = portada.querySelectorAll("img");
+const prevBtn = document.querySelector("#prev");
+const nextBtn = document.querySelector("#next");
+
+let posicionActual = 0;
+
+// Función para mostrar una sola imagen y ocultar las demás
+const mostrarImagen = (indice) => {
+  for (let i = 0; i < imagenes.length; i++) {
+    if (i === indice) {
+      imagenes[i].classList.remove("hide");
+    } else {
+      imagenes[i].classList.add("hide");
+    }
+  }
+};
+
+// Mostrar la primera imagen y ocultar las demás al cargar la página
+mostrarImagen(posicionActual);
+
+// Evento de clic en el botón "Anterior"
+prevBtn.addEventListener("click", () => {
+  if (posicionActual > 0) {
+    posicionActual--;
+  } else {
+    posicionActual = imagenes.length - 1;
+  }
+  mostrarImagen(posicionActual);
+});
+
+// Evento de clic en el botón "Siguiente"
+nextBtn.addEventListener("click", () => {
+  if (posicionActual < imagenes.length - 1) {
+    posicionActual++;
+  } else {
+    posicionActual = 0;
+  }
+  mostrarImagen(posicionActual);
+});
+
++(function () {
   const university = "UOC";
   console.log(`Hello, ${university}!`);
-} )();
+})();
